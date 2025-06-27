44ac33da66e3d61337b78f055725f9962d9a5156
alif1009 <lifrizqulah@gamil.com>
2025-06-27 18:02:06 +0700

    COBA DI CEK LAGI SEMUA NYA

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -13,7 +13,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7 7-7" />
             </svg>
-            <span>Beranda</span>
+            <span>Landing Page</span>
         </a>
     </div>
 
