b91a24a413908a4c57456af6abfef933e8875b76
alif1009 <lifrizqulah@gamil.com>
2025-06-27 17:59:58 +0700

    NI COBA CEK LAGI

--- resources/views/seminarPTN.blade.php
+++ resources/views/seminarPTN.blade.php
@@ -62,7 +62,6 @@
                 class="absolute right-0 mt-2 w-48 bg-white border rounded-md shadow-lg z-50">
                 <a href="{{ route('profile.profil') }}"
                     class="block px-4 py-2 text-sm text-black hover:bg-gray-100">Profile Saya</a>
-                <a href="#" class="block px-4 py-2 text-sm text-black hover:bg-gray-100">Pengaturan</a>
                 <form method="POST" action="#">
                     @csrf
                     <button type="submit" class="w-full text-left px-4 py-2 text-sm text-red-600 hover:bg-gray-100">Log
